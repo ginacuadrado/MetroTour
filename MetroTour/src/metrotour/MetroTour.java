313a1ee67107737708b4a0e9717ac5fa9a3b3f06
Gina Cuadrado <gcuadrado@correo.unimet.edu.ve>
2019-03-24 21:47:20 -0400

    GetData View logo, jlabels and jtextfields

---
+++ MetroTour/src/metrotour/MetroTour.java
@@ -0,0 +1,20 @@
+
+package metrotour;
+
+import GUI.GetData;
+
+/**
+ *
+ * @author pc ready
+ */
+public class MetroTour {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GetData g = new GetData();
+        g.setVisible(true);
+    }
+    
+}
